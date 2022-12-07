c30bfe3e3393a44be8cfbf188ba109794454d4b4
miz <mizu710tr@gmail.com>
2022-12-07 10:34:32 +0900

    add front code

---
+++ front/src/views/GraphView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="homeGraph">
+    <h1>温度計/高緯度計グラフ</h1>
+    <ul>
+      <li v-for="a in test" :key="a">{{ a }}</li>
+      <li v-for="a in test" :key="a">{{ a }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {},
+  data: () => {
+    return {
+      test: ["a", "b", "c"],
+      game: ["bb", "aa"],
+      endgame: "tesl",
+    };
+  },
+});
+</script>
