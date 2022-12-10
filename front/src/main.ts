9dca2efec502ad299a2b6a1fe466dbed6bc2e607
miz <mizu710tr@gmail.com>
2022-12-11 07:21:30 +0900

    追加 vuetify

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).use(router).mount("#app");
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(router).use(vuetify).mount("#app");
